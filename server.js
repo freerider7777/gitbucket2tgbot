0a96e89f34d30426a05339e7b5602b8bdbfa5108
Pavel Biryukov <freerider77@yandex.ru>
2024-02-13 19:25:58 +0300

    Add Issue link

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 ﻿/**
- * @module GitBucket/TC 2 TG bot
+ * @module GitBucket/TeamCity Webhook 2 Telegram bot
  * @license Apache License Version 2.0
  *
  * @author Pavel Biryukov
+ * @copyright Copyright (c) Pavel Biryukov 2023
  */
 const express = require("express");
 const req = require("express/lib/request");
@@ -35,20 +36,24 @@ function checkDayTime() {
 function sendWebHook(request) {
 	var myHeaders = new Headers();
 	myHeaders.append("Content-Type", "application/json");
-	const action = request.action;
+	const action = request.action || '';
 	const commentBody = (request.comment && request.comment.body) === '+' ? ' (Approved!!!)' : '';
 	const comment = request.comment && `<a href=\"${request.comment.html_url}\">Comment<b>${commentBody}</b></a>` || '';
 	const pr = request.pull_request && `<a href=\"${request.pull_request.html_url}\">Pull request</a>` || '';
+	const issue = request.issue && `Issue: <a href=\"${request.issue.html_url}\">${request.issue.title}</a>` || '';
+	const txt = `Event in GitBucket
+${action}
+${comment}
+${issue}
+${pr}
+Отправил(а) ${request.sender.login} 
+Репозиторий <a href=\"${request.repository.html_url}\">${request.repository.name}</a>`;
+const cleanedText = txt.replace(/\n{2,}/g, '\n');
 
 	var raw = JSON.stringify({
 	  chat_id: CHAT_ID,
 	  parse_mode: "HTML",
-	  text: `Event in GitBucket
-${action}
-${comment}
-${pr}
-Sent by ${request.sender.login} 
-Repository <a href=\"${request.repository.html_url}\">${request.repository.name}</a>`
+	  text: cleanedText
 	});
 	
 	var requestOptions = {
